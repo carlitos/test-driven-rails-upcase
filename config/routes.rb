460f7e6e923fa7654b06d65beda63226393facd6
Carlos Moisés Castro Jiménez <carmoicast@gmail.com>
2022-04-18 18:45:14 -0500

    Add completion resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'todos#index'
   resources :todos, only: [:new, :create, :index]
   resources :sessions, only: [:new, :create]
+  resources :completions, only: [:create]
 end
