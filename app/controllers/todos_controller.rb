2578bf409214f8b7d6b4757ae1d79fe391f3b46a
Carlos Moisés Castro Jiménez <carmoicast@gmail.com>
2022-04-18 15:10:44 -0500

    Add where for passing new sign_ins

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,7 @@ class TodosController < ApplicationController
   before_action :authenticate
 
   def index
-    @todos = Todo.all
+    @todos = Todo.where(email: session[:current_email])
   end
 
   def new
@@ -11,7 +11,7 @@ class TodosController < ApplicationController
   end
 
   def create
-    @todo = Todo.create(todo_params)
+    @todo = Todo.create(todo_params.merge( email: session[:current_email] ) )
     redirect_to todos_path
   end
 
