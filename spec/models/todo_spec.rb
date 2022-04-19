d6a28daf317e056be7d719e842b10527e3e1d389
Carlos Moisés Castro Jiménez <carmoicast@gmail.com>
2022-04-18 19:12:21 -0500

    Add spec of completed todos

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -13,3 +13,13 @@ describe "#completed?" do
   end
 
 end
+
+describe "#completed!" do
+  it "update completed_at" do
+    todo = Todo.create!(completed_at: nil)
+    todo.completed!
+    todo.reload
+    expect(todo).to be_completed
+  end
+
+end
