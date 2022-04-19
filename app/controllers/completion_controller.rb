f5e1cf49229845a446271b96190e8604842a963b
Carlos Moisés Castro Jiménez <carmoicast@gmail.com>
2022-04-18 19:10:48 -0500

    Rename controller

---
+++ app/controllers/completion_controller.rb
@@ -0,0 +1,14 @@
+class CompletionController < ApplicationController
+
+  def create
+    todo.touch :completed_at
+    redirect_to todos_path
+  end
+
+  private
+
+  def todo
+    current_user.todos.find(params[:todo_id])
+  end
+
+end
